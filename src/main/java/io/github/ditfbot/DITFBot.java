b4ba7f55f72a4c45c7f4ab82eb2a677192c4303b
Norberts Kakste <norberts.kakste@sparq.eu>
2020-09-28 13:25:30 +0300

    config.properties handling.
    DITFBotConfig responsibilities:
    * Read config.properties file
    * Save config.properties state in DITFBotConfig class object.

--- src/main/java/io/github/ditfbot/DITFBot.java
+++ src/main/java/io/github/ditfbot/DITFBot.java
@@ -1,8 +1,23 @@
 package io.github.ditfbot;
 
+import io.github.ditfbot.config.DITFBotConfig;
+
+import java.io.IOException;
+
 public class DITFBot {
+    public static DITFBotConfig config;
 
     public static void main(String[] args) {
+        // Read BOT Configuration from config.properties (resources folder)
+        try {
+            // Set config and fail in-case file is corrupted / doesn't exist
+            config = DITFBotConfig.getConfig();
+            config.printConfig();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            System.exit(1);
+        }
+
         System.out.println("Starting DITFBot...");
     }
 }
---
+++ src/main/java/io/github/ditfbot/config/DITFBotConfig.java
@@ -0,0 +1,33 @@
+package io.github.ditfbot.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class DITFBotConfig {
+    public final String discordToken;
+
+    public DITFBotConfig(String discordToken) {
+        this.discordToken = discordToken;
+    }
+
+    public void printConfig() {
+        System.out.println("Token: " + this.discordToken);
+    }
+
+    public static DITFBotConfig getConfig() throws IOException {
+        InputStream input = DITFBotConfig.class.getClassLoader().getResourceAsStream("config.properties");
+
+        Properties prop = new Properties();
+
+        if (input == null) {
+            System.out.println("Sorry, unable to find config.properties");
+            return null;
+        }
+
+        //load a properties file from class path, inside static method
+        prop.load(input);
+
+        return new DITFBotConfig(prop.getProperty("DiscordToken"));
+    }
+}
